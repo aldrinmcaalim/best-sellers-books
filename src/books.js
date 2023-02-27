9cef66ac481798c2666e439e273953453e626c08
aldrinmcaalim <aldrinmcaalim@gmail.com>
2023-02-27 16:37:13 -0500

    updated json file

---
+++ src/books.js
@@ -0,0 +1,24 @@
+export const books = [
+  {
+    author: "Ron DeSantis",
+    title: "The Courage to Be Free: Florida's Blueprint for America's Revival",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81QOUISS7VL._AC_UL600_SR600,400_.jpg",
+    id: 1,
+  },
+  {
+    author: "Deborah Hopkinson",
+    title: "My Little Golden Book About Betty White",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81gtzoeueoS._AC_UL600_SR600,400_.jpg",
+    id: 2,
+  },
+  {
+    author: "James Clear",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg",
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
+import { books } from "./books";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import Book from "./Book";
+
+function App() {
+  // const getBook = (id) => {
+  //   const book = books.find((book) => book.id === id);
+  //   console.log(book);
+  // };
+
+  // getBook(2);
+
+  return (
+    <React.Fragment>
+      <h1>Amazon Best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </React.Fragment>
+  );
+}
+
+// const EventExamples = () => {
+//   const handleFormInput = (e) => {
+//     console.log(e.target.value);
+//   };
+//   const handleButtonClick = () => {
+//     alert("handle button click");
+//   };
+//   const handleFormSubmission = (e) => {
+//     e.preventDefault();
+//     console.log("form submitted");
+//   };
+//   return (
+//     <section>
+//       <form onSubmit={handleFormSubmission}>
+//         <h2>Typical form</h2>
+//         <input
+//           type="text"
+//           name="example"
+//           onChange={handleFormInput}
+//           style={{ margin: "1rem 0" }}
+//         />
+//         <button type="submit" onClick={handleButtonClick}>
+//           submit form
+//         </button>
+//       </form>
+//       <button type="button" onClick={handleButtonClick}>
+//         Click me
+//       </button>
+//     </section>
+//   );
+// };
+
+// const Image = (props) => {
+//   return <img src={props.props.image} alt={props.props.title} />;
+// };
+
+// const Title = (props) => {
+//   return <h2>{props.title}</h2>;
+// };
+
+// const Author = (props) => {
+//   return <h4>{props.author}</h4>;
+// };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
